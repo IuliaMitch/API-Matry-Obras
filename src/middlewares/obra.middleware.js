951a6efc52eac0ec20c99948fa9ef957281e5cda
IuliaMitch <noipasz@gmail.com>
2022-04-25 15:26:43 -0300

    criação de middleware e atualização das rotas

--- src/middlewares/obra.middleware.js
+++ src/middlewares/obra.middleware.js
@@ -1,3 +1,4 @@
+const { Template } = require("ejs");
 const mongoose = require("mongoose");
 
 const validId = (req, res, next) => {
@@ -9,17 +10,28 @@ const validId = (req, res, next) => {
 };
 
 const validObjectBody = (req, res, next) => {
-    const obra = req.body;
+  const obra = req.body;
   if (!obra || !obra.nota || !obra.nome || !obra.sinopse || !obra.foto) {
-    return res
-      .status(400)
-      .send({ message: "Envie todos os campos das obras!" });
+    return res.status(400).send({ message: "Envie todos os campos!" });
+  }
+  if (obra.nota > 5 || obra.nota < 1) {
+    return res.status(400).send({message: "Envie uma nota válida"})
   }
 
-    next();
+  next();
+};
+
+const validObjectBodyCart = (req, res, next) => {
+  const cart = req.body;
+  carrinho.forEach((item) => {
+    if (!item || !item.obraId || !item.quantidade) {
+      return res.status(400).send({ message: "Envie todos os campos!" });
+    }
+  });
 };
 
 module.exports = {
-    validId,
-    validObjectBody,
-}
+  validId,
+  validObjectBody,
+  validObjectBodyCart,
+};
--- src/routes/obras.route.js
+++ src/routes/obras.route.js
@@ -1,11 +1,19 @@
 const router = require("express").Router();
 const controllerObras = require("../controllers/obras.controller");
 const { validId, validObjectBody } = require("../middlewares/obra.middleware");
+const controllerCart = require("../controllers/cart.controller")
 
 router.get("/all-obras", controllerObras.findAllObrasController);
 router.get("/one-obra/:id", validId, controllerObras.findByIdObraController);
 router.post("/create-obra", validObjectBody, controllerObras.createObraController);
 router.put("/update-obra/:id", validId, validObjectBody, controllerObras.updateByIdObraController);
 router.delete("/delete-obra/:id", validId, controllerObras.deleteByIdObraController);
+router.get("/all-cart", controllerCart.findAllCartController);
+router.post("/create-cart", controllerCart.createManyItemsCartController);
+router.delete("/finish-cart", controllerCart.deleteAllItemsCartController)
+
+
+
 
 module.exports = router;
+
